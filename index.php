d304060081df42fcc96f7ab6176434406731972d
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-07-31 01:48:58 +0100

    Update branch and add PHP test file

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Servidor PHP Test Site</title>
+    </head>
+
+    <body>
+        <h1>It Works<?= " for PHP" ?>!</h1>
+
+        <?= phpinfo() ?>
+    </body>
+</html>
